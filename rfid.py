ee665996ea77268792b849c981c76b2dc77fd0b0
Muhammet Ali Öztürk <mali_1234@outlook.com.tr>
2022-06-07 19:37:26 +0300

    ilk

--- rfid.py
+++ rfid.py
@@ -11,13 +11,4 @@ def write_read(x):
 while True:
     num = input("numara giriniz: ") # Taking input from user
     value = write_read(num)
-    print("kartın içindeki şifrelenmemiş veri: ",value)
-    s = "Ahmet-Ali-Suzen"
-    b = s.encode("UTF-8")
-    e = base64.b32encode(b)
-    s1 = e.decode("UTF-8")
-    print("şifrelenmiş veri: ", s1)
-    b1 = s1.encode("UTF-8")
-    d = base64.b32decode(b1)
-    s2 = d.decode("UTF-8")
-    print("şifresi çözülmüş veri: ",s2)
+    print(value)
